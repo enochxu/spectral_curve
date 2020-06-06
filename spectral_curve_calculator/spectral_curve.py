66b4b67ed970c4079e9ef7d3804305e5d0b0ea66
Enoch Xu <Enoch.t.xu@gmail.com>
2020-06-05 18:30:19 -0700

    changed location

---
+++ spectral_curve_calculator/spectral_curve.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    substrate_color = [95.65867622736137, 41.89439137669053, 77.11017914916165]
+    monolayer_graphene_color = [92.09239342300415, 36.123467574326696, 75.95457000069112]
+    
+
+if __name__ == '__main__':
+    main()
