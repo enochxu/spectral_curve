d12945c93ac232a35cff026c99bca3bc926d9571
Enoch Xu <Enoch.t.xu@gmail.com>
2020-08-15 12:17:11 -0700

    pipenv + some structure changes

---
+++ spectral_curve_calculator/color_correction.py
@@ -0,0 +1,20 @@
+SUBSTRATE = [95.65867622736137, 41.89439137669053, 77.11017914916165]
+MONOLAYER_GR = [
+    92.09239342300415,
+    36.123467574326696,
+    75.95457000069112,
+]
+
+
+def color_correct(correct_color, image):
+    sample[0] *= calculatedSubstrate[0]/actualSubstrate[0]
+    sample[1] *= calculatedSubstrate[1]/actualSubstrate[1]
+    sample[2] *= calculatedSubstrate[2]/actualSubstrate[2]
+    return sample
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
---
+++ spectral_curve_calculator/image_corrections.py
@@ -0,0 +1,37 @@
+import numpy as np
+from cv2 import cv2
+
+
+# undo gamma correction
+def gamma_expand(image):
+    # conversion to float for increased accuracy
+    u = image.astype("float32") / 255
+    conds = [u <= 0.04045, u > 0.04045]
+    funcs = [lambda u: u / 12.92, lambda u: np.power(((u + 0.055) / 1.055), 2.4)]
+    linearized_image = np.piecewise(u, conds, funcs) * 255
+    # conversion back to uint8 for opencv
+    return linearized_image.astype("uint8")
+
+
+# method creates vignette with a gaussian kernel(can be improved)
+def de_vignette(image, sigma):
+    # setting the gaussian deviation to match the size of the image
+    rows, cols = image.shape[:2]
+    kernel_x = cv2.getGaussianKernel(cols, sigma)
+    kernel_y = cv2.getGaussianKernel(rows, sigma)
+    # sigma = gaussian standard deviation
+
+    kernel = kernel_y * kernel_x.T
+    mask = kernel / kernel.max()
+
+    vignette = np.zeros_like(image)
+    for i in range(3):
+        vignette[:, :, i] = image[:, :, i] * (1 / mask)
+    return vignette
+
+
+def thresholding(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (9, 9), 0)
+    retval, th = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    return th
